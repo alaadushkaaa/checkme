972fe232cb8d13dfad8be6313340256275b5e01b
Anya Aladina <anka.aladjina@yandex.ru>
2025-04-11 11:37:54 +0300

    General user initialization (refs #2)

--- kotlinServer/src/main/kotlin/checkme/service/InitUsers.kt
+++ kotlinServer/src/main/kotlin/checkme/service/InitUsers.kt
@@ -9,15 +9,23 @@ import dev.forkhandles.result4k.Success
 const val GENERAL_NAME = "Admin"
 
 fun OperationHolder.initGeneralUser(config: AppConfig) {
+    println("Я здесь был")
     when (val generalUser = this.userOperations.fetchUsersByRole(Role.ADMIN)) {
-        is Success -> generalUser.value
-        is Failure ->
-            this.userOperations
-                .createUser(
-                    GENERAL_NAME,
-                    GENERAL_NAME,
-                    config.authConfig.generalPass,
-                    Role.ADMIN
-                )
+        is Success -> {
+            if (generalUser.value.isEmpty()) {
+                this.userOperations
+                    .createUser(
+                        GENERAL_NAME,
+                        GENERAL_NAME,
+                        config.authConfig.generalPass,
+                        Role.ADMIN
+                    )
+            } else {
+                generalUser.value
+            }
+        }
+
+        is Failure -> generalUser.reason
+
     }
 }
