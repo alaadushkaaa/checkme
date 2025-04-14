2e3d8b8bb84ce0092ef4226b6fe5978a197b722f
Anya Aladina <anka.aladjina@yandex.ru>
2025-04-14 20:39:31 +0300

    Ktlint correction (refs #2)

--- kotlinServer/src/test/kotlin/checkme/db/users/InsertUserTest.kt
+++ kotlinServer/src/test/kotlin/checkme/db/users/InsertUserTest.kt
@@ -1,9 +1,12 @@
 package checkme.db.users
 
-import checkme.db.*
+import checkme.db.TestcontainerSpec
+import checkme.db.appConfiguredPasswordHasher
+import checkme.db.validName
+import checkme.db.validPass
+import checkme.db.validSurname
 import checkme.domain.accounts.Role
 import checkme.domain.models.User
-import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 
