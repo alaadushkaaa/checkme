a007b20d13cbba94b6bf81ca2c7b049803a832f5
Anya Aladina <anka.aladjina@yandex.ru>
2025-04-25 20:33:59 +0300

    Rebasing (refs #1)

--- kotlinServer/src/main/kotlin/checkme/Server.kt
+++ kotlinServer/src/main/kotlin/checkme/Server.kt
@@ -1,7 +1,6 @@
 package checkme
 
 import checkme.config.AppConfig
-import checkme.db.DatabaseOperationsHolder
 import checkme.db.utils.createJooqContext
 import checkme.domain.operations.OperationHolder
 import checkme.service.initApplication
