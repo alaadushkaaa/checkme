88977515e91d548ed64ca55e0889899054a84e00
Anya Aladina <anka.aladjina@yandex.ru>
2025-07-08 14:43:05 +0300

    Add task functions (refs #9)

--- kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/SupportingTaskFunctions.kt
+++ kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/SupportingTaskFunctions.kt
@@ -11,11 +11,22 @@ import dev.forkhandles.result4k.Success
 internal fun addTask(
     task: Task,
     taskOperations: TaskOperationsHolder
-) : Result<Task, CreateTaskError> {
+): Result<Task, CreationTaskError> {
     return when (
         val newTask = taskOperations.createCheck(
             task.name,
-
+            task.criterions,
+            task.answerFormat,
+            task.description,
         )
-    )
+    ) {
+        is Success -> Success(newTask.value)
+        is Failure -> when (newTask.reason) {
+            CreateTaskError.UNKNOWN_DATABASE_ERROR -> Failure(CreationTaskError.UNKNOWN_DATABASE_ERROR)
+        }
+    }
+}
+
+enum class CreationTaskError(val errorText: String) {
+    UNKNOWN_DATABASE_ERROR("Something happened. Please try again later or ask for help"),
 }
