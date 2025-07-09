e63b590511d085d684f0a05eed5ac6875653bd7d
Anya Aladina <anka.aladjina@yandex.ru>
2025-07-09 14:01:44 +0300

    Add base task handler(refs #9)

--- kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/AddTaskHandler.kt
+++ kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/AddTaskHandler.kt
@@ -1,20 +1,53 @@
 package checkme.web.tasks.handlers
 
 import checkme.db.tasks.TasksOperations
+import checkme.domain.models.Task
+import checkme.domain.operations.tasks.TaskOperationsHolder
+import checkme.web.auth.handlers.SignUpError
+import checkme.web.lenses.GeneralWebLenses.idOrNull
 import checkme.web.lenses.TaskLenses
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Success
 import org.http4k.core.*
 import org.http4k.lens.MultipartForm
 
 class AddTaskHandler(
-    private val tasksOperations: TasksOperations,
+    private val tasksOperations: TaskOperationsHolder,
 ) : HttpHandler {
     override fun invoke(request: Request): Response {
+        val objectMapper = jacksonObjectMapper()
+        val taskId = request.idOrNull()
         val form: MultipartForm = TaskLenses.multipartFormFieldsAll(request)
-        val taskName = TaskLenses.nameField(form).value
-        val description = TaskLenses.descriptionField(form).value
-        val criterions = TaskLenses.criterionsField(form).value
-        val files = TaskLenses.filesField(form)
-        val newTask =
-            TODO("Not yet implemented")
+        return when (
+            val validatedNewTask = form.validateForm(taskId)
+        ) {
+            is Failure -> Response(Status.INTERNAL_SERVER_ERROR).body(
+                objectMapper.writeValueAsString(
+                    mapOf("error" to validatedNewTask.reason)
+                )
+            )
+
+            is Success -> {
+                when (val newTask =
+                    addTask(
+                        validatedNewTask.value,
+                        tasksOperations
+                    )
+                ) {
+                    is Success -> Response(Status.OK).body(
+                        objectMapper.writeValueAsString(
+                            mapOf("taskId" to newTask.value.id)
+                        )
+                    )
+
+                    is Failure -> Response(Status.INTERNAL_SERVER_ERROR).body(
+                        objectMapper.writeValueAsString(
+                            mapOf("error" to newTask.reason.errorText)
+                        )
+                    )
+                }
+            }
+        }
     }
 }
