bbf8216db9568362625fb147d04bebd383360d1d
Anya Aladina <anka.aladjina@yandex.ru>
2025-07-09 14:01:44 +0300

    Add base task handler(refs #9)

--- kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/SupportingTaskFunctions.kt
+++ kotlinServer/src/main/kotlin/checkme/web/tasks/handlers/SupportingTaskFunctions.kt
@@ -1,12 +1,21 @@
 package checkme.web.tasks.handlers
 
+import checkme.db.generated.enums.AnswerFormat
 import checkme.db.tasks.TasksOperations
+import checkme.domain.checks.Criterion
+import checkme.domain.models.FormatOfAnswer
 import checkme.domain.models.Task
 import checkme.domain.operations.tasks.CreateTaskError
 import checkme.domain.operations.tasks.TaskOperationsHolder
+import checkme.web.lenses.GeneralWebLenses.idOrNull
+import checkme.web.lenses.TaskLenses
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Success
+import org.http4k.lens.MultipartForm
+import org.http4k.lens.WebForm
 
 internal fun addTask(
     task: Task,
@@ -26,7 +35,32 @@ internal fun addTask(
         }
     }
 }
+//todo add all checks for validate task
+fun MultipartForm.validateForm(
+    taskId: Int?
+) : Result<Task, ValidateTaskError> {
+    val jacksonMapper = jacksonObjectMapper()
+    val taskName = TaskLenses.nameField(this).value
+    val description = TaskLenses.descriptionField(this).value
+    val criterions : Map<String, Criterion> =
+        jacksonMapper.readValue<Map<String, Criterion>>(TaskLenses.criterionsField(this).value)
+    val answerFormat =  jacksonMapper.readValue<FormatOfAnswer>(TaskLenses.answerFormatField(this).value)
+    val files = TaskLenses.filesField(this)
+    return Success(Task(
+        id = taskId?:-1,
+        name = taskName,
+        criterions = criterions,
+        answerFormat = answerFormat,
+        description = description
+    ))
+
+}
 
 enum class CreationTaskError(val errorText: String) {
+    UNKNOWN_DATABASE_ERROR("Something happened. Please try again later or ask for help"),
+}
+
+enum class ValidateTaskError(val errorText: String) {
+
     UNKNOWN_DATABASE_ERROR("Something happened. Please try again later or ask for help"),
 }
