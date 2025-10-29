4a51194e1516ec7d70aa2b1b83a4a6027a0fabb5
Anya Aladina <anka.aladjina@yandex.ru>
2025-10-29 16:50:33 +0300

    Added bundle operations classes (refs #44)

---
+++ kotlinServer/src/main/kotlin/checkme/domain/operations/bundles/FetchBundle.kt
@@ -0,0 +1,58 @@
+package checkme.domain.operations.bundles
+
+import checkme.domain.models.Bundle
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import org.jooq.exception.DataAccessException
+
+class FetchBundleById(
+    private val fetchBundleById: (Int) -> Bundle?,
+) : (Int) -> Result4k<Bundle, BundleFetchingError> {
+
+    override fun invoke(bundleId: Int): Result4k<Bundle, BundleFetchingError> =
+        try {
+            when (val bundle = fetchBundleById(bundleId)) {
+                is Bundle -> Success(bundle)
+                else -> Failure(BundleFetchingError.NO_SUCH_BUNDLE)
+            }
+        } catch (_: DataAccessException) {
+            Failure(BundleFetchingError.UNKNOWN_DATABASE_ERROR)
+        }
+}
+
+class FetchAllBundles(
+    private val fetchAllBundles: () -> List<Bundle>?,
+) : () -> Result4k<List<Bundle>, BundleFetchingError> {
+
+    override fun invoke(): Result4k<List<Bundle>, BundleFetchingError> =
+        try {
+            when (val bundles = fetchAllBundles()) {
+                is List<Bundle> -> Success(bundles)
+                else -> Failure(BundleFetchingError.NO_SUCH_BUNDLE)
+            }
+        } catch (_: DataAccessException) {
+            Failure(BundleFetchingError.UNKNOWN_DATABASE_ERROR)
+        }
+}
+
+class FetchHiddenBundles(
+    private val fetchHiddenBundles: () -> List<Bundle>?,
+) : () -> Result4k<List<Bundle>, BundleFetchingError> {
+
+    override fun invoke(): Result4k<List<Bundle>, BundleFetchingError> =
+        try {
+            when (val bundles = fetchHiddenBundles()) {
+                is List<Bundle> -> Success(bundles)
+                else -> Failure(BundleFetchingError.NO_SUCH_BUNDLE)
+            }
+        } catch (_: DataAccessException) {
+            Failure(BundleFetchingError.UNKNOWN_DATABASE_ERROR)
+        }
+}
+
+
+enum class BundleFetchingError {
+    UNKNOWN_DATABASE_ERROR,
+    NO_SUCH_BUNDLE,
+}
