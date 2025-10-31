44ea54d77cea0c1f2f63115245a06695cc337a16
Anya Aladina <anka.aladjina@yandex.ru>
2025-10-31 10:49:00 +0300

    Added modify bundle class (refs #44)

--- kotlinServer/src/main/kotlin/checkme/domain/operations/bundles/BundleOperationHolder.kt
+++ kotlinServer/src/main/kotlin/checkme/domain/operations/bundles/BundleOperationHolder.kt
@@ -1,4 +1,26 @@
 package checkme.domain.operations.bundles
 
-class BundleOperationHolder {
+import checkme.domain.models.Bundle
+import checkme.domain.operations.dependencies.bundles.BundlesDatabase
+import dev.forkhandles.result4k.Result
+
+class BundleOperationHolder(
+    private val bundleDatabase: BundlesDatabase,
+){
+    val fetchBundleById: (Int) -> Result<Bundle, BundleFetchingError> =
+        FetchBundleById {
+            bundleId: Int
+            ->
+            bundleDatabase.selectBundleById(bundleId)
+        }
+
+    val fetchAllBundles: () -> Result<List<Bundle>, BundleFetchingError> =
+        FetchAllBundles {
+            bundleDatabase.selectAllBundles()
+        }
+
+    val fetchHiddenBundles: () -> Result<List<Bundle>, BundleFetchingError> =
+        FetchHiddenBundles {
+            bundleDatabase.selectHiddenBundles()
+        }
 }
