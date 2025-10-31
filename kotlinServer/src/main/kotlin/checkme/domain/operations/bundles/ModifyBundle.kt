9da20f69c94fa1cdac8c2ea116c56773e6b7c4a8
Anya Aladina <anka.aladjina@yandex.ru>
2025-10-31 10:49:00 +0300

    Added modify bundle class (refs #44)

--- kotlinServer/src/main/kotlin/checkme/domain/operations/bundles/ModifyBundle.kt
+++ kotlinServer/src/main/kotlin/checkme/domain/operations/bundles/ModifyBundle.kt
@@ -1,3 +1,85 @@
 package checkme.domain.operations.bundles
 
-class ModifyBundle
+import checkme.domain.models.Bundle
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import org.jooq.exception.DataAccessException
+
+class ModifyBundle(
+    private val updateBundle: (
+        name: String,
+        tasks: Map<Int, Int>,
+        isActual: Boolean
+    ) -> Bundle?,
+) : (String, Map<Int, Int>, Boolean) -> Result4k<Bundle, ModifyBundleError> {
+    override fun invoke(
+        name: String,
+        tasks: Map<Int, Int>,
+        isActual: Boolean
+    ): Result4k<Bundle, ModifyBundleError> =
+        when (
+            val editedBundle = updateBundle(
+                name,
+                tasks,
+                isActual
+            )
+        ) {
+            is Bundle -> Success(editedBundle)
+            else -> Failure(ModifyBundleError.UNKNOWN_DATABASE_ERROR)
+        }
+}
+
+class ModifyBundleActuality(
+    private val updateBundleActuality: (
+      bundle: Bundle
+    ) -> Bundle?,
+) : (Bundle) -> Result4k<Bundle, ModifyBundleError> {
+    override fun invoke(
+        bundle: Bundle
+    ): Result4k<Bundle, ModifyBundleError> =
+        when (
+            val editedBundle = updateBundleActuality(
+                bundle
+            )
+        ) {
+            is Bundle -> Success(editedBundle)
+            else -> Failure(ModifyBundleError.UNKNOWN_DATABASE_ERROR)
+        }
+}
+
+class RemoveBundle(
+    private val selectBundleById: (bundleId: Int) -> Bundle?,
+    private val removeBundle: (Int) -> Int?,
+) : (Bundle) -> Result<Int, BundleRemovingError> {
+    override fun invoke(bundle: Bundle): Result<Int, BundleRemovingError> {
+        return try {
+            when {
+                bundleNotExists(bundle.id) -> Failure(BundleRemovingError.BUNDLE_NOT_EXISTS)
+                else -> when (removeBundle(bundle.id)) {
+                    is Int -> Success(bundle.id)
+                    else -> Failure(BundleRemovingError.UNKNOWN_DELETE_ERROR)
+                }
+            }
+        } catch (_: DataAccessException) {
+            Failure(BundleRemovingError.UNKNOWN_DATABASE_ERROR)
+        }
+    }
+
+    private fun bundleNotExists(bundleId: Int): Boolean =
+        when (selectBundleById(bundleId)) {
+            is Bundle -> false
+            else -> true
+        }
+}
+
+enum class ModifyBundleError {
+    UNKNOWN_DATABASE_ERROR,
+}
+
+enum class BundleRemovingError {
+    UNKNOWN_DATABASE_ERROR,
+    UNKNOWN_DELETE_ERROR,
+    BUNDLE_NOT_EXISTS,
+}
