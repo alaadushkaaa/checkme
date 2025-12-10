b2dfbed924f27445fabac450215bd7e9da98e91a
Anya Aladina <anka.aladjina@yandex.ru>
2025-12-10 12:54:25 +0300

    Deleted files for another task(resolves #52)

--- kotlinServer/src/main/kotlin/checkme/web/bundles/forms/BundleTasksResponse.kt
+++
@@ -1,9 +0,0 @@
-package checkme.web.bundles.forms
-
-import checkme.domain.models.Bundle
-import checkme.domain.models.TaskAndOrder
-
-data class BundleTasksResponse(
-    val bundle: Bundle,
-    val tasks: List<TaskAndOrder>,
-)
